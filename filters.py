af5a84a64d35282324acacf478d24a045dd2ea31
Aitana <aitana.waelbroeck@hotmail.com>
2021-11-28 18:28:48 +0100

    filters.py  version_1

--- filters.py
+++ filters.py
@@ -1,7 +1,132 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Nov 28 17:04:28 2021
+Created on Sun Nov 28 18:23:37 2021
 
-@author: henry papapatos
+@author: Aitana Waelbroeck Boix
 """
+import numpy as np
 
+# Description: Extended Kalman Filter example (two-wheeled mobile robot)
+
+# Supress scientific notation when printing NumPy arrays
+np.set_printoptions(precision=3,suppress=True)
+ 
+# A matrix
+A_k_minus_1 = np.array([[1.0,  0,   0],
+                       [  0, 1.0,   0],
+                       [  0,   0, 1.0]])
+
+# 
+process_noise_v_k_minus_1 = np.array([0.01,0.01,0.003])
+     
+# State model noise covariance matrix Q_k
+Q_k = np.array([[1.0,   0,   0],
+                [  0, 1.0,   0],
+                [  0,   0, 1.0]])
+                 
+# Measurement matrix H_k
+H_k = np.array([[1.0,  0,   0],
+                [  0,1.0,   0],
+                [  0,  0, 1.0]])
+                         
+# Sensor measurement noise covariance matrix R_k
+R_k = np.array([[1.0,   0,    0],
+                [  0, 1.0,    0],
+                [  0,    0, 1.0]])  
+                 
+# Sensor noise
+sensor_noise_w_k = np.array([0.07,0.07,0.04])
+ 
+def getB(yaw, deltak):
+    """
+    # Author: Addison Sears-Collins
+    # https://automaticaddison.com
+    Calculates and returns the B matrix
+    3x2 matix -> number of states x number of control inputs
+    The control inputs are the forward speed and the
+    rotation rate around the z axis from the x-axis in the 
+    counterclockwise direction.
+    [v,yaw_rate]
+    Expresses how the state of the system [x,y,yaw] changes
+    from k-1 to k due to the control commands (i.e. control input).
+    :param yaw: The yaw angle (rotation angle around the z axis) in rad 
+    :param deltak: The change in time from time step k-1 to k in sec
+    """
+    B = np.array([  [np.cos(yaw)*deltak, 0],
+                    [np.sin(yaw)*deltak, 0],
+                    [0, deltak]])
+    return B
+ 
+def ekf(z_k_observation_vector, state_estimate_k_minus_1, 
+        control_vector_k_minus_1, P_k_minus_1, dk):
+    """
+    # Author: Addison Sears-Collins
+    # https://automaticaddison.com
+    Extended Kalman Filter. Fuses noisy sensor measurement to 
+    create an optimal estimate of the state of the robotic system.
+         
+    INPUT
+        :param z_k_observation_vector The observation from the Odometry
+            3x1 NumPy Array [x,y,yaw] in the global reference frame
+            in [meters,meters,radians].
+        :param state_estimate_k_minus_1 The state estimate at time k-1
+            3x1 NumPy Array [x,y,yaw] in the global reference frame
+            in [meters,meters,radians].
+        :param control_vector_k_minus_1 The control vector applied at time k-1
+            3x1 NumPy Array [v,v,yaw rate] in the global reference frame
+            in [meters per second,meters per second,radians per second].
+        :param P_k_minus_1 The state covariance matrix estimate at time k-1
+            3x3 NumPy Array
+        :param dk Time interval in seconds
+             
+    OUTPUT
+        :return state_estimate_k near-optimal state estimate at time k  
+            3x1 NumPy Array ---> [meters,meters,radians]
+        :return P_k state covariance_estimate for time k
+            3x3 NumPy Array                 
+    """
+    ######################### Predict #############################
+    # Predict the state estimate at time k based on the state 
+    # estimate at time k-1 and the control input applied at time k-1.
+    state_estimate_k = A_k_minus_1 @ (
+            state_estimate_k_minus_1) + (
+            getB(state_estimate_k_minus_1[2],dk)) @ (
+            control_vector_k_minus_1) + (
+            process_noise_v_k_minus_1)
+             
+    print(f'X_est State Estimate Before EKF={state_estimate_k}')
+             
+    # Predict the state covariance estimate based on the previous
+    # covariance and some noise
+    P_k = A_k_minus_1 @ P_k_minus_1 @ A_k_minus_1.T + (
+            Q_k)
+         
+    ################### Update (Correct) ##########################
+    # Calculate the difference between the actual sensor measurements
+    # at time k minus what the measurement model predicted 
+    # the sensor measurements would be for the current timestep k.
+    measurement_residual_y_k = z_k_observation_vector - (
+            (H_k @ state_estimate_k) + (
+            sensor_noise_w_k))
+ 
+    print(f'Z Measurements={z_k_observation_vector}')
+             
+    # Calculate the measurement residual covariance
+    S_k = H_k @ P_k @ H_k.T + R_k
+         
+    # Calculate the near-optimal Kalman gain
+    # We use pseudoinverse since some of the matrices might be
+    # non-square or singular.
+    K_k = P_k @ H_k.T @ np.linalg.pinv(S_k)
+         
+    # Calculate an updated state estimate for time k
+    state_estimate_k = state_estimate_k + (K_k @ measurement_residual_y_k)
+     
+    # Update the state covariance estimate for time k
+    P_k = P_k - (K_k @ H_k @ P_k)
+     
+    # Print the best (near-optimal) estimate of the current state of the robot
+    print(f'X_est State Estimate After EKF={state_estimate_k}')
+ 
+    # Return the updated state and covariance estimates
+    return state_estimate_k, P_k
