ff32ea85671940250ea969ebb798c598811d7473
Aitana <aitana.waelbroeck@hotmail.com>
2021-12-04 16:27:29 +0100

    Kalman a jour camera hidden

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ Created on Sun Nov 28 17:03:23 2021
 @author: henry papapatos
 """
 import numpy as np
+import sys
+#sys.path.append(r'C:\Users\Usuario\OneDrive - epfl.ch\Documents\EPFL\Basics of mobile robotics\PROJET\Github\Mobile_robotics')
 import filters 
 import math
 import cv2 
@@ -14,10 +16,7 @@ TRESH_DIST = 3 #mm
 CAMERA = 1 # Camera
 
 def main():
-                     
-    # The estimated state vector at time t-1 in the global reference frame.
-    # [x_t_minus_1, y_t_minus_1, yaw_t_minus_1]
-    
+                      
     cap=cv2.VideoCapture(CAMERA) # Either 0 or 1, front camera or external cam
 
     # Get 1st frame of video 
@@ -25,13 +24,16 @@ def main():
     
     #cv2.imwrite('mettre le path ici', frame_init) POUR TUNING PAR ELIOTT
     
+    # Extract vertexes, goals, thymio's start position and orientation from first frame
     start_pos, vertexes, goals_pos, px_to_mm = computer_vision.Init(frame_init)
     
     goal_list = [] # Path planning
     
-    ###### APPELER get_vision_position##########
+    # The estimated state vector at time t-1 in the global reference frame.
+    # [x_t_minus_1, y_t_minus_1, yaw_t_minus_1]
     x_est_t_minus_1, hidden = computer_vision.vision(frame_init, px_to_mm)
-     
+    #x_est_t_minus_1 = np.array([0.0,0.0,0.0])
+    
     # The control input vector at time t-1 in the global reference frame.
     # [v, yaw_rate]
     u_t_minus_1 = np.array([0 ,0])
@@ -41,12 +43,10 @@ def main():
                             [  0,0.1,   0],
                             [  0,  0, 0.1]])
 
-# Extract vertexes, goals, thymio's start position and orientation from first frame 
-# .
-# .
-# . 
-    
 
+    #update le dt car sinon kalman marche pas
+    dt = 0 ; #A voir a modifier, Aitana
+    
     while len(goal_list)!=0:
         
         ret, frame = cap.read()
@@ -55,19 +55,26 @@ def main():
         obs_vector_z_t, hidden = computer_vision.vision(frame, px_to_mm)
         
         print(f'Timestep measurement={obs_vector_z_t}')
-        
-        #######ADD TIME MEASURE TWO KALMAN CALLS#######
-        dt = 0 ; #A voir a modifier, Aitana
-        
+
         ####### ADD HIDDEN CONDITION !!!!#############
         # Run the Extended Kalman Filter and store the 
         # near-optimal state and covariance estimates
-        optimal_state_estimate_t, covariance_estimate_t = filters.ekf(
-            obs_vector_z_t, # Most recent sensor measurement
-            x_est_t_minus_1, # Our most recent estimate of the state
-            u_t_minus_1, # Our most recent control input
-            P_t_minus_1, # Our most recent state covariance matrix
-            dt) # Time interval
+        if hidden:
+            #should I pudate covariance or not 
+            _, _, optimal_state_estimate_t= filters.ekf(
+                obs_vector_z_t, # Most recent sensor measurement
+                x_est_t_minus_1, # Our most recent estimate of the state
+                u_t_minus_1, # Our most recent control input
+                P_t_minus_1, # Our most recent state covariance matrix
+                dt) # Time interval
+            print('Camera hidden')
+        else: 
+            optimal_state_estimate_t, covariance_estimate_t, _= filters.ekf(
+                obs_vector_z_t, # Most recent sensor measurement
+                x_est_t_minus_1, # Our most recent estimate of the state
+                u_t_minus_1, # Our most recent control input
+                P_t_minus_1, # Our most recent state covariance matrix
+                dt) # Time interval
         
         if np.linalg.norm(optimal_state_estimate_t-goal_list[0]) < TRESH_DIST:
             goal_list.pop(0)
