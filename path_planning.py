f9761b833c17ec86a8781c7c5e12c6aadf7b5bdd
Henry papadatos <papadatoshenry@gmail.com>
2021-12-11 17:03:34 +0100

    comment path planning

--- path_planning.py
+++ path_planning.py
@@ -13,41 +13,54 @@ import six
 import sys
 sys.modules['sklearn.externals.six'] = six
 import mlrose
-
-
-def draw(image, start, goal, obstacle):
-    cv2.circle(image, start,radius=0, color=(0,255,0), thickness=5)
-
-    for current_goal in goal:
-        cv2.circle(image, current_goal,radius=0, color=(0,255,255), thickness=5) 
-
-    #draw obstacle
-    cv2.fillPoly(image, obstacle, color=(0,0,255))
     
  
 def Astar(start, goal, obstacle,draw = False, image = None):
-    polys = []
-    for cobstacle in obstacle:
+    """
+    Effectuate the Astar algorithm to find the shortest distance 
+    and its corresponding path between each pair of goals.
+    This library is used: https://github.com/TaipanRex/pyvisgraph
+    
+    param start: position of the robot at the beginning
+    param goal: list of position of goals where the robot has to go
+    param obstacle: list of position of the corners of the extended obstacles
+    param draw: if True, all the shortest path between pair of goals/start will be ploted
+    param image: the image on which we draw the path
+    
+    return dist_list: list of the lengths of all shortest paths
+    return path_list: list of intermediate coordinates composing each of shortest path
+    """
+    
+    polys = [] 
+    #creates the list of obstacle with the format needed by pyvisgraph library
+    for cobstacle in obstacle: 
         cpolys = []
         for cpos in cobstacle:
-            cpolys.append(vg.Point(cpos[0],cpos[1]))
+            cpolys.append(vg.Point(cpos[0],cpos[1])) 
         polys.append(cpolys)
     
+    #point is the list of goals in wich we insert the start position
     point = goal.copy()
     point.insert(0, start)
     dist_list=[]
     path_list=[]
 
+    #create the visibility graph
     g = vg.VisGraph()
     g.build(polys)
         
+    #for each pair of point (in point list), we compute the shortest path
     for i in range(len(point)):
         for k in range(len(point)-i-1):
             j = k+i+1
+            #computes the shortest path between point i and j
             shortest = g.shortest_path(vg.Point(point[i][0], point[i][1]), vg.Point(point[j][0], point[j][1]))
+            #add the shortest path into the path_list
             path_list.append([i, j, shortest])
+            #add the length of the sortest path in the dist_list
             dist_list.append((i, j, path_length(shortest)))
             
+            #if param draw = True, draw all the sortest paths on param image
             if draw:
                 h = 0
                 for h in range (len(shortest)-1):
@@ -58,7 +71,13 @@ def Astar(start, goal, obstacle,draw = False, image = None):
     return dist_list, path_list
 
 def path_length(path):
+    """
+    Computes the length of a path
     
+    param path: positions of all the points composing the path
+    
+    return distance: length of the path
+    """
     distance = 0
     for i in range(len(path)-1):
         distance += math.sqrt((path[i].x-path[i+1].x)**2 + (path[i].y-path[i+1].y)**2)
@@ -66,10 +85,23 @@ def path_length(path):
     return distance
 
 def tsp(dist_list, path_list, length, draw = False, image = None):
+    """
+    Solve the travelling salesman problem using a genetic algorithm.
+    The library used is mlrose: https://github.com/gkhayes/mlrose
+    
+    param dist_list: list of the lengths of all shortest paths
+    param path_list: list of intermediate coordinates composing each of shortest path
+    param length: number of points to travel to
+    param draw: if True, all the shortest path between pair of goals/start will be ploted
+    param image: the image on which we draw the path
+    
+    return optimal_trajectory: list of points that compose the optimal path (expressed in pixel coordinates)
+    """
     
     optimal_trajectory=[]
 
-    # Initialize fitness function object using dist_list
+    # Initialize fitness function object using dist_list (dist_list is the length of the 
+    # pairwise shortest path computed in Astar function)
     fitness_dists = mlrose.TravellingSales(distances = dist_list)
     # Define optimization problem object
     problem_fit2 = mlrose.TSPOpt(length, fitness_fn = fitness_dists, maximize = False)
@@ -103,6 +135,7 @@ def tsp(dist_list, path_list, length, draw = False, image = None):
         
     optimal_trajectory.append(np.array([path_list[0][2][0].x,path_list[0][2][0].y]))  
     
+    #if param draw = True, draw the sortest path on param image
     if draw:
         for i in range(len(optimal_trajectory)):
             start = optimal_trajectory[i].astype(int)
@@ -112,14 +145,29 @@ def tsp(dist_list, path_list, length, draw = False, image = None):
                 stop = optimal_trajectory[i+1].astype(int)
                 
             image = cv2.line(image,start, stop, color=(255, 153, 255), thickness = 1)
+    
+   
     return optimal_trajectory
 
 def get_optimal_path(start, goal, obstacle,conversion_factor = 1, draw = False, image = None):
+    """
+    compute the optimal path to join all the goals and then go back to the start position 
+    while avoiding the obstacles
     
+    param start: position of the robot at the beginning
+    param goal: list of position of goals where the robot has to go
+    param obstacle: list of position of the corners of the extended obstacles
+    param conversion_factor: ratio to convert pixels in mm
+    param draw: if True, all the shortest path between pair of goals/start will be ploted
+    param image: the image on which we draw the path
+    
+    return optimal_trajectory: list of points that compose the optimal path (expressed in mm coordinates)
+    """
     dist_list, path_list = Astar(start, goal, obstacle)
     
     optimal_path = tsp(dist_list, path_list, len(goal)+1, draw, image)
     
+    #converts the position from pixel to mm
     for i in range(len(optimal_path)):
         optimal_path[i] = optimal_path[i] * conversion_factor
         
