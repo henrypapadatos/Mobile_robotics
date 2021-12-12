7b827302c916ce0d25543b217c6d82417656cf6a
JulPel <juliette.pelissier@epfl.ch>
2021-12-12 13:46:55 +0100

    clean of obstacle_avoidance.py Report local avoidance

--- obstacle_avoidance.py
+++ obstacle_avoidance.py
@@ -11,6 +11,26 @@ AVG_SPEED = 70
 
 def obstacle_avoidance(horz_prox, speedl, speedr, verbose = False):
     
+    '''
+    Computes the speed of the robot depending of the distance to the obstacle,
+    based on an Artificial Neural Network with memory.
+    
+    INPUT
+        :param horz_prox 
+            1x7 NumPy Array collecting the values from the horizontal 
+            proximity sensors
+        :param speedl, speedr 
+            respective current speeds of left and right motors
+             
+    OUTPUT
+        :return y 
+            1x2 NumPy Array [speedl, speedr] of speeds of the wheels [mm/s]
+        :return state
+            state of the robot 
+            1 --> obstacle avoidance
+            0 --> goal tracking
+    '''
+    
     w_l = [20,  20, -20, -20, -20,  0, 0, 1, 0]
     w_r = [-22, -22, -22,  22, 22, 0, 0, 0, 1]
 
@@ -18,6 +38,7 @@ def obstacle_avoidance(horz_prox, speedl, speedr, verbose = False):
     
     y = [speedl,speedr]
    
+    # Memory
     x[7] = y[0]/CST_SCALE
     x[8] = y[1]/CST_SCALE
     
@@ -44,6 +65,7 @@ def obstacle_avoidance(horz_prox, speedl, speedr, verbose = False):
     state = 1
     
     if y[0]<=SPEED_TRH and y[1]<=SPEED_TRH:
+        # Back to goal tracking
         state = 0
         
     return y, state
